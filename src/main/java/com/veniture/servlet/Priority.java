b823ca52a29d6d2577cf63589a789bf9a67e15fa
odinson8 <berkkarabacak@gmail.com>
2019-11-21 10:45:43 +0300

    priority servleti eklendi

---
+++ src/main/java/com/veniture/servlet/Priority.java
@@ -0,0 +1,67 @@
+package com.veniture.servlet;
+
+import com.atlassian.jira.bc.issue.IssueService;
+import com.atlassian.jira.bc.issue.search.SearchService;
+import com.atlassian.jira.bc.project.ProjectService;
+import com.atlassian.jira.config.ConstantsManager;
+import com.atlassian.jira.security.JiraAuthenticationContext;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
+import com.atlassian.sal.api.net.RequestFactory;
+import com.atlassian.templaterenderer.TemplateRenderer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Scanned
+public class Priority extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(Priority.class);
+
+    @JiraImport
+    private IssueService issueService;
+    @JiraImport
+    private ProjectService projectService;
+    @JiraImport
+    private SearchService searchService;
+    @JiraImport
+    private TemplateRenderer templateRenderer;
+    @JiraImport
+    private JiraAuthenticationContext authenticationContext;
+    @JiraImport
+    private ConstantsManager constantsManager;
+    @JiraImport
+    private RequestFactory requestFactory;
+    String action;
+
+    private static final String priority_ISSUES_TEMPLATE = "/templates/priority.vm";
+
+    public Priority(IssueService issueService, ProjectService projectService,
+                       SearchService searchService,
+                       TemplateRenderer templateRenderer,
+                       JiraAuthenticationContext authenticationContext,
+                       ConstantsManager constantsManager,
+                       RequestFactory requestFactory) {
+        this.issueService = issueService;
+        this.projectService = projectService;
+        this.searchService = searchService;
+        this.templateRenderer = templateRenderer;
+        this.authenticationContext = authenticationContext;
+        this.constantsManager = constantsManager;
+        this.requestFactory = requestFactory;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        action = Optional.ofNullable(req.getParameter("action")).orElse("");
+
+        resp.setContentType("text/html;charset=utf-8");
+        templateRenderer.render(priority_ISSUES_TEMPLATE,  resp.getWriter());
+    }
+}
